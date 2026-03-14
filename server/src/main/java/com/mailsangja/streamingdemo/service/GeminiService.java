5b3ecbda47a2a299ddd5ab51d991e60397896673
휘래 김 <hramst0618@gmail.com>
2026-03-14 19:19:04 +0900

    fix: jackson 의존성 수정

--- server/src/main/java/com/mailsangja/streamingdemo/service/GeminiService.java
+++ server/src/main/java/com/mailsangja/streamingdemo/service/GeminiService.java
@@ -1,7 +1,7 @@
 package com.mailsangja.streamingdemo.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
